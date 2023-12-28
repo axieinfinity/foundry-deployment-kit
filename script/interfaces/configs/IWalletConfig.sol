edff367bb5b4398217d72927d6e661d8484eab84
TuDo1403 <tudo.dev@gmail.com>
2023-12-28 18:03:18 +0700

    feat: expose interface for signing EthSignMessage or SignTypedDataV4

--- script/interfaces/configs/IWalletConfig.sol
+++ script/interfaces/configs/IWalletConfig.sol
@@ -12,4 +12,28 @@ interface IWalletConfig {
   function trezorPrefix() external view returns (string memory);
 
   function deployerEnvLabel() external view returns (string memory);
+
+  function ethSignMessage(address by, string memory message, WalletOption walletOption)
+    external
+    returns (bytes memory sig);
+
+  function ethSignMessage(string memory message) external returns (bytes memory sig);
+
+  function envEthSignMessage(address by, string memory message, string memory envLabel)
+    external
+    returns (bytes memory sig);
+
+  function envSignTypedDataV4(address by, string memory filePath, string memory envLabel)
+    external
+    returns (bytes memory sig);
+
+  function trezorEthSignMessage(address by, string memory message) external returns (bytes memory sig);
+
+  function trezorSignTypedDataV4(address by, string memory filePath) external returns (bytes memory sig);
+
+  function signTypedDataV4(address by, string memory filePath, WalletOption walletOption)
+    external
+    returns (bytes memory sig);
+
+  function signTypedDataV4(string memory filePath) external returns (bytes memory sig);
 }
