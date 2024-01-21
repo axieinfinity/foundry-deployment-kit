511b10b929d9d07356180e799efe831c0fe7e0ad
TuDo1403 <tudo.dev@gmail.com>
2024-01-21 17:11:56 +0700

    script(BaseMigration): remove duplicate build share config

--- script/interfaces/configs/IMigrationConfig.sol
+++ script/interfaces/configs/IMigrationConfig.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.19;
 
 interface IMigrationConfig {
+  function areSharedArgumentsStored() external view returns (bool);
+
   function setRawSharedArguments(bytes calldata migrationConfig) external;
 
   function getRawSharedArguments() external view returns (bytes memory);
