91e2078012e3036e375b9a4f4359c4265c7b23c3
TuDo1403 <tudo.dev@gmail.com>
2024-05-19 23:03:39 +0700

    script: combine export_address.sh to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-source ./export-address.sh
-
 verify_arg=""
 extra_argument=""
 
@@ -8,6 +6,134 @@ op_command=""
 network_name=""
 is_broadcast=false
 should_verify=false
+
+# Define the deployments folder by concatenating it with the child folder
+root="deployments/"
+
+start_time=$(date +%s)
+
+for folder in "$root"/*; do
+    # If exported_address.toml exists, delete it
+    if [ -f "$folder"/exported_address ]; then
+        rm "$folder"/exported_address
+    fi
+
+    # Create a new exported_address file
+    touch "$folder"/exported_address
+
+    for file in "$folder"/*.json; do
+
+        # Check if the file exists and is a regular file
+        if [ -f "$file" ] && [ "$(basename "$file")" != ".chainId" ] && [ "$(basename "$file")" != "exported_address" ]; then
+            ((index++))
+            (
+                # Extract address from the JSON file
+                contractAddress=$(jq -r '.address' "$file")
+                # Extract contractName from file name without the extension
+                contractName=$(basename "$file" .json)
+
+                # Check if contractName and address are not empty
+                if [ -n "$contractName" ]; then
+                    # Write to file the contractName and address
+                    echo "$contractName@$contractAddress" >>"$folder"/exported_address
+                else
+                    echo "Error: Missing contractName or address in $file"
+                fi
+            ) &
+        fi
+
+        # Check if index is a multiple of 10, then wait
+        if [ $((index % 10)) -eq 0 ]; then
+            wait
+        fi
+    done
+done
+
+wait
+
+end_time=$(date +%s)
+echo "Export address in deployment folder: $((end_time - start_time)) seconds"
+
+index=0
+
+for arg in "$@"; do
+    case $arg in
+    --trezor)
+        extra_argument+=trezor@
+        ;;
+    --no-postcheck)
+        set -- "${@/#--no-postcheck/}"
+        extra_argument+=no-postcheck@
+        ;;
+    --verify)
+        should_verify=true
+        set -- "${@/#--verify/}"
+        ;;
+    -f | --fork-url)
+        network_name=${@:index+2:1}
+        extra_argument+="network.${network_name}@"
+
+        set -- "${@/#-f/}"
+        set -- "${@/#--fork-url/}"
+        set -- "${@/#$network_name/}"
+        ;;
+    --fork-block-number)
+        fork_block_number=${@:index+2:1}
+        extra_argument+="fork-block-number.${fork_block_number}@"
+
+        set -- "${@/#--fork-block-number/}"
+        set -- "${@/#$fork_block_number/}"
+        ;;
+    --broadcast)
+        is_broadcast=true
+        ;;
+    *) ;;
+    esac
+    index=$((index + 1))
+done
+
+should_verify=$([[ $should_verify == true && $is_broadcast == true ]] && echo true || echo false)
+
+if [[ $should_verify == true ]]; then
+    extra_argument+=generate-artifact@
+fi
+
+if [[ $should_verify == true ]] && [[ ! $network_name == "ronin-mainnet" ]] && [[ ! $network_name == "ronin-testnet" ]]; then
+    verify_arg="--verify --retries 5"
+fi
+
+echo "Should Verify Contract: $should_verify"
+
+# Remove the @ character from the end of extra_argument
+extra_argument="${extra_argument%%@}"
+
+## Check if the private key is stored in the .env file
+if [[ ! $extra_argument == *"sender"* ]] && [[ ! $extra_argument == *"trezor"* ]]; then
+    source .env
+
+    if [[ $MAINNET_PK == op* ]] || [[ $TESTNET_PK == op* ]] || [[ $LOCAL_PK == op* ]]; then
+        op_command="op run --env-file="./.env" --"
+    fi
+fi
+
+calldata=$(cast calldata 'run()')
+start_time=$(date +%s)
+
+${op_command} forge script ${verify_arg} ${@} -g 200 --sig 'run(bytes,string)' ${calldata} "${extra_argument}"
+
+# Check if the command was successful
+if [ $? -eq 0 ]; then
+    if [[ $should_verify == true ]]; then
+        if [[ $network_name == "ronin-mainnet" ]] || [[ $network_name == "ronin-testnet" ]]; then
+            echo "Verifying contract..."
+            ${op_command} yarn hardhat sourcify --endpoint https://sourcify.roninchain.com/server --network ${network_name}
+        fi
+    fi
+fi
+
+end_time=$(date +%s)
+
+echo "Execution time: $((end_time - start_time))s"
 
 for arg in "$@"; do
     case $arg in
